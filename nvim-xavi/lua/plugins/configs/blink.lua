9ed885651d02d12f3f414be2126ed1f1774ed8ac
Rheisen <rheisen@pm.me>
2026-01-15 05:01:51 -0500

    Switch to more emac style completion keymappings

--- nvim-xavi/lua/plugins/configs/blink.lua
+++ nvim-xavi/lua/plugins/configs/blink.lua
@@ -25,7 +25,11 @@ local M = {
 		-- C-k: Toggle signature help (if signature.enabled = true)
 		--
 		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "default" },
+		keymap = {
+			preset = "default",
+			["<C-e>"] = { "accept", "fallback" },
+			["<C-y>"] = { "hide", "fallback" },
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
