bf853a6ede29f15bd798cc87ed6c3d885531c713
Rheisen <rheisen@pm.me>
2026-01-15 19:44:00 -0500

    Add diffview plugin and remove keybindings from neogit plugin

--- nvim-xavi/lua/plugins/configs/neogit.lua
+++ nvim-xavi/lua/plugins/configs/neogit.lua
@@ -3,13 +3,10 @@ local M = {
 	lazy = true,
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
-		-- "sindrets/diffview.nvim", -- optional - Diff integration
+		"sindrets/diffview.nvim", -- optional - Diff integration
 		"folke/snacks.nvim", -- optional
 	},
 	cmd = "Neogit",
-	keys = {
-		{ "<leader>vg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-	},
 }
 
 return M
