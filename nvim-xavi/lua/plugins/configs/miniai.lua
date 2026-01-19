8e51b4c22dae5de0be4862b3ac61babc5244e435
Rheisen <rheisen@pm.me>
2026-01-19 16:51:20 -0500

    WIP mini ai plugin

---
+++ nvim-xavi/lua/plugins/configs/miniai.lua
@@ -0,0 +1,26 @@
+local M = {
+	"nvim-mini/mini.ai",
+	version = "*",
+	event = "VeryLazy",
+	opts = function()
+		local ai = require("mini.ai")
+		return {
+			custom_textobjects = {
+				o = ai.gen_spec.treesitter({ -- code block
+					a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+					i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+				}),
+				f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+				c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+				t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+				d = { "%f[%d]%d+" }, -- digits
+				e = { -- Word with case
+					{ "%u[%l%d]+%f[^%l%d]", "%f[%S][%l%d]+%f[^%l%d]", "%f[%P][%l%d]+%f[^%l%d]", "^[%l%d]+%f[^%l%d]" },
+					"^().*()$",
+				},
+			},
+		}
+	end,
+}
+
+return M
